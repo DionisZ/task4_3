00d90f1050a26e85915c28251d8dbd3580f10f56
DionisZ <dionis270391z@gmail.com>
2018-04-01 23:40:38 +0300

    stable_02

--- task4_3.sh
+++ task4_3.sh
@@ -4,30 +4,29 @@
 #$2 count of backups
 
 if [ "$USER" != "root" ]; then
-        echo "The user $USER  doesn't have root access!" 1>&2
-        exit 1
+	echo "The user $USER  doesn't have root access!" 1>&2
+	exit 1
 fi
 
 if [ "$#" -ne 2 ]; then
-        echo "WRONG numbers of arguments!" 1>&2
-        exit 1
+	echo "WRONG numbers of arguments!" 1>&2
+	exit 1
 fi
 
 if ! [[ "$2" =~ ^[0-9]*$ ]]; then
-        echo "$2 is not a number" 1>&2
+	echo "$2 is not a number" 1>&2
 fi
 
 if [ ! -d "$1" ]; then
-        echo "The $1 directory does not exist!" 1>&2
-        exit 1
+	echo "The $1 directory does not exist!" 1>&2
+	exit 1
 fi
 
 if [ ! -d "/tmp/backups" ]; then
-mkdir /tmp/backups
+	mkdir /tmp/backups
 fi
 
 name=$(echo "$1" | sed 's/\//-/g' | sed 's/\-$//g' | sed 's/^.//g')
-updatedb
 N=$(find /tmp/backups/ -name "$name*" | wc -l)
 find /tmp/backups/ -name "$name*" 2> /dev/null | sort | head -$(($N - $2 + 1 )) 2> /dev/null | sed 's/\ /\\\ /g' |xargs rm -f
 tar -czf /tmp/backups/"$name"\_$(date +%Y-%m-%d_%H:%M:%S).tar.gz $1 2> /dev/null
